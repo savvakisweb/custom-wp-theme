8510905b49cb59b957d3445895de8439b6b8fa6f
savvakisweb <manos.savvakis@outlook.com>
2025-03-06 20:55:32 +0200

    first commit

---
+++ page.php
@@ -0,0 +1,9 @@
+<?php
+    while (have_posts()) {
+        the_post(); ?>
+        <h2> <?php the_title(); ?> </h2>
+        <h2> This is a page, not a post. </h2>
+        <?php the_content(); ?>
+        <?php
+    }
+?>
