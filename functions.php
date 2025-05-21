bcfbb8137fc7674e2c58a93543cde52e9c51a87f
savvakisweb <manos.savvakis@outlook.com>
2025-05-21 23:17:16 +0300

    Convert footer menus to dynamic

--- functions.php
+++ functions.php
@@ -11,6 +11,8 @@ function sweb_files() {
 add_action('wp_enqueue_scripts', 'sweb_files');
 
 function university_features() {
+    register_nav_menu('footerLocationOne', 'Footer Location One');
+    register_nav_menu('footerLocationTwo', 'Footer Location Two');
     add_theme_support('title-tag');
 }
 
